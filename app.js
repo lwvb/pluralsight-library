940286f76c9169f9262b2441fa7bba4136ddcc26
lwvb <leendertvanbeelen@gmail.com>
2016-03-04 23:19:18 +0100

    Basic static routes
    Routes for static files and a temp route for the index template

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ var app = express();
 
 var port = 5000;
 
+app.use(express.static('public'));
+// TODO: temp route to make things work
+app.use(express.static('src/views'));
+
+
 app.get('/', function(request, response) {
   response.send('Hello world');
 });
+
 app.get('/books', function(request, response) {
   response.send('Hello books');
 });
+
 app.listen(port, function(err) {
   console.log('running server on port ' + port);
 });
