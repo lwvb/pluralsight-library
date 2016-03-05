317e4217b11f880b892f3cbe187b972c4ce2df54
lwvb <leendertvanbeelen@gmail.com>
2016-03-05 01:10:58 +0100

    Added nodemon
    Start node using a gulp task and auto restart after a change

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jsFiles = ['*.js', 'src/**/*.js'];
+var nodemon = require('gulp-nodemon');
 
 gulp.task('style', function() {
   gulp.src(jsFiles)
       .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+      .pipe(jshint.reporter('jshint-stylish', {
+          verbose: true
+      }));
 });
 
 gulp.task('inject', function() {
   var wiredep = require('wiredep').stream;
-  var options = {
+  var wiredepOptions = {
     ignorePath: "../../bower_components/",
     fileTypes: {
       html: {
@@ -20,8 +23,26 @@ gulp.task('inject', function() {
         }
       }
     }
-  }
+  };
+
   return gulp.src('src/views/*.html')
-    .pipe(wiredep(options))
-    .pipe(gulp.dest('src/views'));
+             .pipe(wiredep(wiredepOptions))
+             .pipe(gulp.dest('src/views'));
+});
+
+
+gulp.task('serve', ['style'], function() {
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      'PORT': 5000
+    },
+    watch: jsFiles
+  };
+
+  return nodemon(options)
+    .on('restart', function(ev) {
+      console.log('Restarting.....');
+    });
 });
